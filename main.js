92239c5eafb524b8f01003d9752342060389d73d
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-06 15:13:41 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let ourTeam = [
+    { Nome: "Wayne Barnett", Ruolo: "Founder & CEO", foto: "wayne-barnett-founder-ceo.jpg" },
+    { Nome: "Angela Caroll", Ruolo: "Chief Editor", foto: "angela-caroll-chief-editor.jpg" },
+    { Nome: "Walter Gordon", Ruolo: "Office Manager", foto: "walter-gordon-office-manager.jpg" },
+    { Nome: "Angela Lopez", Ruolo: "Social Media Manager", foto: "angela-lopez-social-media-manager.jpg" },
+    { Nome: "Scott Estrada", Ruolo: "Developer", foto: "scott-estrada-developer.jpg" },
+    { Nome: "Barbara Ramos", Ruolo: "Graphic Designer", foto: "barbara-ramos-graphic-designer.jpg" },
+]
+
