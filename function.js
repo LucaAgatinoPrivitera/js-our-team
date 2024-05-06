166a447649b2ac59c33a09c2e5a444c57b5a7530
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-06 15:38:51 +0200

    Milestone 2 - Finita, mi chiedo se ho fatto bene ad usare il json.stringfy

--- function.js
+++ function.js
@@ -1,9 +1,20 @@
-function milestone2() {
+function milestone1() {
     for (let i = 0; i < ourTeam.length; i++) {
 
         const ourTeamInside = ourTeam[i]; //Così sto creando una costante, che si basa sull'array, ad ogni ciclo si prende i valori di ogni oggetto, quindi da lì becca il nome e il ruolo.
         console.log("Nome in posizione " + i + " dell'array: ", `${ourTeamInside.Nome} ${ourTeamInside.Ruolo}`);
         /*console.log("Nome in posizione " + i + " dell'array: ", `${ourTeamInside.Nome}`, "Ruolo in posizione " + i + " dell'array: " `${ourTeamInside.Ruolo}`); Non gira così*/
-        
     }
+}
+
+function milestone2() {
+    for (let i = 0; i < ourTeam.length; i++) {
+        const ourTeamInside = ourTeam[i]; //Così sto creando una costante, che si basa sull'array, ad ogni ciclo si prende i valori di ogni oggetto, quindi da lì becca il nome e il ruolo.
+        console.log("Nome in posizione " + i + " dell'array: ", `${ourTeamInside.Nome} ${ourTeamInside.Ruolo} ${ourTeamInside.foto}`); //Non è una stringa
+
+
+        myJSON = JSON.stringify(ourTeam); //Trasforma tutto ourTeam in stringa
+        console.log(myJSON); //Print dell'array in formato stringa
+    }
+
 }
