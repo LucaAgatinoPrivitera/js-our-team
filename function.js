66d748c361767dd7c2873c37aa70b381e4d66c82
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-06 15:33:05 +0200

    Milestone 1 - Mi sono sbagliato era la milestone 1

--- function.js
+++ function.js
@@ -4,5 +4,6 @@ function milestone2() {
         const ourTeamInside = ourTeam[i]; //Così sto creando una costante, che si basa sull'array, ad ogni ciclo si prende i valori di ogni oggetto, quindi da lì becca il nome e il ruolo.
         console.log("Nome in posizione " + i + " dell'array: ", `${ourTeamInside.Nome} ${ourTeamInside.Ruolo}`);
         /*console.log("Nome in posizione " + i + " dell'array: ", `${ourTeamInside.Nome}`, "Ruolo in posizione " + i + " dell'array: " `${ourTeamInside.Ruolo}`); Non gira così*/
+        
     }
 }
